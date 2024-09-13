d12b3e72539532b2e5a833c97c9dada988f4a510
Kezia811 <santosferreira804@gmail.com>
2024-09-13 11:27:30 -0300

    Update index.py

--- index.py
+++ index.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello World!"
 
+@app.route('/contacts')
+def contacts():
+    return "Diga o que você quer!"
+
 if __name__ == '__main__':
     app.run(debug=True)
